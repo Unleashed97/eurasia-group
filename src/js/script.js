b5e36895fdf810eac38a415d5c17123d3ee61689
Nichkov Roman <nichkovroman@gmail.com>
2022-10-14 10:47:34 +0300

    feat: add showcase

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
 import * as customFunctions from './modules/functions.js'
 
 // vendors
+import Swiper, { Navigation, Pagination } from 'swiper'
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-btn--next',
+        prevEl: '.swiper-btn--prev',
+    },
+})
