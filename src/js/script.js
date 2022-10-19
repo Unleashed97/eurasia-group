f20e95f0c93144744306cf7d9c4a3533bc82a3f3
Nichkov Roman <nichkovroman@gmail.com>
2022-10-20 00:02:59 +0300

    feat: swiper remove

--- src/js/script.js
+++ src/js/script.js
@@ -3,33 +3,6 @@ customFunctions.handleAccordion()
 customFunctions.handleModal()
 customFunctions.handleBurger()
 
-// vendors
-import Swiper, { Navigation, Pagination } from 'swiper'
-
-// init Swiper:
-const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation, Pagination],
-
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    spaceBetween: 40,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-btn--next',
-        prevEl: '.swiper-btn--prev',
-    },
-})
-
 // support webp check
 
 /*! modernizr 3.6.0 (Custom Build) | MIT *
